2e3c198aa844610c037ab2c3686e87489acc2883
Rubo Vermishayn <161845079+RuVerr@users.noreply.github.com>
2026-07-11 01:47:02 +0400

    Update OrganismHome.tsx
    fix home height

--- app/Components/AtomUI/Organisms/OrganismHome/OrganismHome.tsx
+++ app/Components/AtomUI/Organisms/OrganismHome/OrganismHome.tsx
@@ -66,7 +66,7 @@ export default function OrganismHome() {
     <>
       <MoleculesBackground />
       <div className="container">
-        <div className="home_content py-[20px] flex justify-end items-center gap-5 h-screen max-[1151px]:flex-col justify-center max-[620px]:pt-[800px] max-[442px]:pt-[750px]">
+        <div className="home_content py-[20px] flex justify-end items-center gap-5 h-[100dvh] max-[1151px]:flex-col justify-center max-[620px]:pt-[800px] max-[442px]:pt-[750px]">
           <MoleculesHeader
             moleculesHeading={headingRef}
             moleculesLine={lineRef}
