46a41d0ccd6ce0452004ec199fae943c4b850171
RuVerr <ruvermishyan@gmail.com>
2026-07-19 14:40:52 +0400

    fix background z-index

--- app/Components/AtomUI/Atoms/AtomBackground/AtomBackground.tsx
+++ app/Components/AtomUI/Atoms/AtomBackground/AtomBackground.tsx
@@ -8,7 +8,7 @@ interface AtomBackgroundProp {
 export default function AtomBackground({ backgroundSRC }: AtomBackgroundProp) {
   if (!backgroundSRC) return null;
   return (
-    <div className="pointer-events-none fixed inset-0 z-[0]">
+    <div className="pointer-events-none fixed inset-0 z-[-1]">
       <Image src={backgroundSRC} alt="Legend music hall background image" fill priority className="object-cover" />
     </div>
   );
